9f0eab2304dfa77fd20eb2155bdb0c83d6535cde
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-28 16:29:48 +0300

    create deploy_app.sh

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-
 sudo apt-get update
+wget https://raw.githubusercontent.com/qurname/infra/master/install_ruby.sh -O install_ruby.sh
+bash install_ruby.sh && wget https://raw.githubusercontent.com/qurname/infra/master/install_mongodb.sh -O install_mongodb.sh
+bash install_mongodb.sh && wget https://raw.githubusercontent.com/qurname/infra/master/deploy.sh -O deploy.sh &&
+bash deploy.sh
