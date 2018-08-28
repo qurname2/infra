9c16d638fa488df36c0c96319ba0eb5401ce3991
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-28 16:45:40 +0300

    3

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+curl -sSL https://get.rvm.io | sudo bash -s stable
 source /home/appuser/.rvm/scripts/rvm
 rvm requirements
 rvm install 2.4.1
