4f8bc1ea612eaaa5ec8092b1f397b0b1b01e0e45
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-28 18:06:30 +0300

    7

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/appuser/
 git clone https://github.com/Artemmkin/reddit.git
-cd reddit && . ~/.bashrc && bundle install
-puma -d
+cd reddit && echo 'export PATH="$PATH:/usr/local/rvm/gems/ruby-2.4.1/bin/bin"' >> ~/.bashrc
+. ~/.bashrc && bundle install && puma -d
