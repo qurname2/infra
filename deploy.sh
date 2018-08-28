dddfddf7f4218bfd6a052996985c7e11b962dc1e
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-28 18:26:33 +0300

    asd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/appuser/
 git clone https://github.com/Artemmkin/reddit.git
-cd reddit && echo 'export PATH="$PATH:/usr/local/rvm/"' >> ~/.bashrc
-. ~/.bashrc && bundle install && puma -d
+cd reddit && source /etc/profile && bundle install && puma -d
