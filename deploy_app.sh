2e8494c8ad3f0304e5346756b58e1b7ae70c01c6
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-28 18:35:52 +0300

    finish

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#since i'm using sudo before install_ruby - rvm will be install in multi-ser mode
 sudo apt-get update
 wget https://raw.githubusercontent.com/qurname/infra/master/install_ruby.sh
 sudo bash install_ruby.sh && wget https://raw.githubusercontent.com/qurname/infra/master/install_mongodb.sh
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#because i use sudo before this script, i need add user appuser in group rvm and make . /etc/profile in deploy.sh
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
 sudo usermod -a -G rvm appuser
