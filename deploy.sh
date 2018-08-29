37ff4c05b343864b8019b6b0e6309dafb4426046
Ivan Bakursky <bakyrskiy@gmail.com>
2018-08-30 00:36:59 +0300

    priches2

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/appuser/
-git clone https://github.com/Artemmkin/reddit.git
-cd reddit && source /etc/profile && bundle install && puma -d
-#source /etc/profile && bundle install && puma -d
--- deploy_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-wget https://raw.githubusercontent.com/qurname/infra/master/install_ruby.sh
-bash install_ruby.sh && wget https://raw.githubusercontent.com/qurname/infra/master/install_mongodb.sh
-sudo bash install_mongodb.sh && wget https://raw.githubusercontent.com/qurname/infra/master/deploy.sh  &&
-sudo bash deploy.sh
